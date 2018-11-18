a3ad4878fb1e9aee93476b047fa339aedf36f310
Gabriel L Martinez <g.lorenzo.martinez@gmail.com>
2018-11-18 10:01:56 -0400

    Add calculator request using api with 2 text field

--- class1/ViewController.swift
+++ class1/ViewController.swift
@@ -10,22 +10,50 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var textInputName: UITextField!
+    @IBOutlet weak var textInput1: UITextField!
+    @IBOutlet weak var textInput2: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Modificando el tamaño de text field
-        var frameRect: CGRect = textInputName.frame;
-        frameRect.size.height = 50;
-        textInputName.frame = frameRect;
+//        var frameRect: CGRect = textInputName.frame;
+//        frameRect.size.height = 50;
+//        textInputName.frame = frameRect;
         
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     @IBAction func onClick(_ sender:Any) {
-        let value: String = textInputName.text!
-        let alert = UIAlertController(title: "Hello World!", message: "Welcome \(value)", preferredStyle: .alert);
+        var urlComponents = URLComponents(string: "https://avires.net/suma/")!
+        print(textInput1.text!)
+        urlComponents.queryItems = [
+            URLQueryItem(name: "n1", value: textInput1.text!),
+            URLQueryItem(name: "n2", value: textInput2.text!)
+        ]
+        
+        let request = URLRequest(url: urlComponents.url!)
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error = error {
+                print ("error: \(error)")
+                return
+            }
+            guard let data = data,
+                let response = response as? HTTPURLResponse,
+                (200 ..< 300) ~= response.statusCode,
+                error == nil else {
+                    print ("server error")
+                    return
+            }
+            let responseObj = (try? JSONSerialization.jsonObject(with: data)) as? [String: Any]
+            print(responseObj!)
+            self.showMessage("Hello World!", message: responseObj?["mensaje"] as? String ?? "Message")
+        }
+        task.resume()
+    }
+    
+    func showMessage(_ title:String, message:String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert);
         alert.addAction(UIAlertAction(title: "Close", style: .default, handler: nil));
         self.present(alert, animated: true);
     }
